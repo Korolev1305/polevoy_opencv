45fd582e72aca77269ea86298e94f39c9d1001e7
Maxim Korolev <>
2019-04-04 23:03:22 +0300

    add CMakeLists , dz1,dz2, ,kr (контрольная)

---
+++ kr/kr.cpp
@@ -0,0 +1,70 @@
+//
+//  main.cpp
+//  testopencv
+//
+//  Created by Королев Максим Алексеевич on 21/02/2019.
+//  Copyright © 2019 Королев Максим Алексеевич. All rights reserved.
+//
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+    Mat img(600,600, CV_8UC1);
+    int colorCircle[] = {127,0,255,255,127,0};
+    int colorRect[] = {255,127,0,127,0,255};
+    int k, z, index =0;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            if(i==1){
+                rectangle(img, Point(k,z), Point(k+img.rows/3,z+img.rows/2),Scalar(colorRect[index]),FILLED);
+                index++;
+            } else {
+                rectangle(img, Point(k,z), Point(k+img.rows/3,z+img.rows/2),Scalar(colorRect[index]),FILLED);
+                index++;
+            }
+                k+=img.rows/3;
+        }
+        z+=img.rows/2;
+        k=0;
+    }
+    index = 0;
+    for(int i=0;i<img.cols;i++){
+        for(int j=0;j<img.rows;j++){
+            
+            if ((j==img.rows/6||j==img.rows/2||j==img.rows/6*5)){
+                if(i==img.rows/4){
+                    circle(img, Point(j,i), img.rows/8, Scalar(colorCircle[index]),FILLED);
+                    index++;
+                } else if (i==img.rows/4*3){
+                    circle(img, Point(j,i), img.rows/8, Scalar(colorCircle[index]),FILLED);
+                    index++;
+                }
+            }
+        }
+    }
+    
+    Mat resFirst;
+    Mat resSecond;
+    const float kernelData1[] = {
+        1.0f, 0,
+        0, -1.0f};
+    const Mat kernel1(2, 2, CV_32FC1,(float *)kernelData1);
+    filter2D(img, resFirst, -1, kernel1);
+    
+    const float kernelData2[] = {
+        0, 1.0f,
+        -1.0f, 0};
+    const Mat kernel2(2, 2, CV_32FC1,(float *)kernelData2);
+    filter2D(img, resSecond, -1, kernel2);
+    
+    imshow("after filter", resFirst);
+    imshow("after filter2", resSecond);
+    imshow("result",img);
+    waitKey();
+    destroyAllWindows();
+    return 0;
+
+}
